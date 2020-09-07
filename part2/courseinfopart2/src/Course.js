6cdc0b5a20ea92b95584a4288453120690d5a160
guruhedipurwanto <guruhedipurwanto@gmail.com>
2020-09-07 11:56:59 +0800

    add exercise 2.1 - 2.5: Course Information to submit

---
+++ part2/courseinfopart2/src/Course.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Header = ({ course }) => {
+  return <h2>{course.name}</h2>;
+};
+
+const Total = ({ course }) => {
+  const sum = course.parts.reduce(
+    (total, currentPart) => total + currentPart.exercises,
+    0
+  );
+  return <p className="sum">Number of exercises {sum}</p>;
+};
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      {course.parts.map((part) => {
+        return <Part part={part} key={part.id} />;
+      })}
+    </div>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  );
+};
+
+export default Course;
